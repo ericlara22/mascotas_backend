d40e1cb630db33ea70fd215c5f8ad5efc630c61b
elara22 <ericlaravega@gmail.com>
2022-11-24 11:56:58 -0300

    publicacioncontroller

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const router = require('./router');
 const db = require('./models');
+const cors = require('cors')
 require('dotenv').config();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cors());
 app.use('/', router);
 
 (async () => {
